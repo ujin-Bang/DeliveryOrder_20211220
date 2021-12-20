1a46cd6741fcb405ed0680a61b37cf9e735ffa4a
ujin-Bang <qkddnwls14@gmail.com>
2021-12-20 11:12:56 +0900

    임시 - 피자가게 프래그먼트 초안 끝

--- app/src/main/java/com/neppplus/deliveryorder_20211220/fragments/PizzaStoreLIstFragment.kt
+++ app/src/main/java/com/neppplus/deliveryorder_20211220/fragments/PizzaStoreLIstFragment.kt
@@ -16,4 +16,9 @@ class PizzaStoreLIstFragment: Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_pizza_store_list, container, false)
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+//        동작 관련 코드 작성 예정
+    }
 }
