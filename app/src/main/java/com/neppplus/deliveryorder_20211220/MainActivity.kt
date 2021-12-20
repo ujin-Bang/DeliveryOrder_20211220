5fe6ca7e0b64f676d98b214243ad75e9e111777a
ujin-Bang <qkddnwls14@gmail.com>
2021-12-20 11:25:51 +0900

    메인 탭 레이아웃 / 뷰페이져 연결 코드 한줄 작성

--- app/src/main/java/com/neppplus/deliveryorder_20211220/MainActivity.kt
+++ app/src/main/java/com/neppplus/deliveryorder_20211220/MainActivity.kt
@@ -23,5 +23,7 @@ class MainActivity : AppCompatActivity() {
         mvpa = MainViewPagerAdapter(supportFragmentManager)
         mainViewPager.adapter = mvpa
 
+        mainTabLayout.setupWithViewPager(mainViewPager)
+
     }
 }
