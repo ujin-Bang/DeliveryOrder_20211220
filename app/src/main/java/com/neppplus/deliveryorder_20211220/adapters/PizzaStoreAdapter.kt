0c9d3d5e5fb74ee2ae04f7f2543d311bd09e41ec
ujin-Bang <qkddnwls14@gmail.com>
2021-12-21 23:12:59 +0900

    리스트뷰에 뿌려질 데이터/ UI태그 소환

--- app/src/main/java/com/neppplus/deliveryorder_20211220/adapters/PizzaStoreAdapter.kt
+++ app/src/main/java/com/neppplus/deliveryorder_20211220/adapters/PizzaStoreAdapter.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import com.neppplus.deliveryorder_20211220.R
 import com.neppplus.deliveryorder_20211220.datas.StoreData
 
@@ -24,6 +26,13 @@ class PizzaStoreAdapter(
 
         }
         val row = tempRow!!
+
+        val data = mList[position]
+
+        val imgLogo = row.findViewById<ImageView>(R.id.imgLogo)
+        val txtStoreName = row.findViewById<TextView>(R.id.txtStoreName)
+
+
 
         return row
     }
