52d22487e4563ba13f5994d6f4807dee532fa278
ujin-Bang <qkddnwls14@gmail.com>
2021-12-22 12:03:26 +0900

    넘겨준 가게 데이터 받아서  > UI에 반영

--- app/src/main/java/com/neppplus/deliveryorder_20211220/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/neppplus/deliveryorder_20211220/ViewStoreDetailActivity.kt
@@ -2,8 +2,14 @@ package com.neppplus.deliveryorder_20211220
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.neppplus.deliveryorder_20211220.datas.StoreData
+import kotlinx.android.synthetic.main.activity_view_store_detail.*
 
 class ViewStoreDetailActivity : AppCompatActivity() {
+
+   lateinit var mStoreData : StoreData
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_store_detail)
@@ -15,7 +21,15 @@ class ViewStoreDetailActivity : AppCompatActivity() {
     fun setupEvents() {
 
     }
-    fun setValues(){
+    fun setValues() {
+
+        mStoreData = intent.getSerializableExtra("store") as StoreData
+
+        txtStoreName.text = mStoreData.name
+        txtPhoneNum.text = mStoreData.phonNum
+
+        Glide.with(this).load(mStoreData.logoUrl).into(imgLogo)
 
     }
+
 }
