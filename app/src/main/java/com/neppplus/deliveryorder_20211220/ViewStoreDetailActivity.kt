2658f7d45abfd80d0b0088b6f7e93fe8794a4acd
ujin-Bang <qkddnwls14@gmail.com>
2021-12-22 12:21:27 +0900

    실제 권한 획득 > 전화 연결까지

--- app/src/main/java/com/neppplus/deliveryorder_20211220/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/neppplus/deliveryorder_20211220/ViewStoreDetailActivity.kt
@@ -1,8 +1,14 @@
 package com.neppplus.deliveryorder_20211220
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import com.neppplus.deliveryorder_20211220.datas.StoreData
 import kotlinx.android.synthetic.main.activity_view_store_detail.*
 
@@ -22,7 +28,27 @@ class ViewStoreDetailActivity : AppCompatActivity() {
 
         btnCall.setOnClickListener {
 
+            val pl = object : PermissionListener {
+                override fun onPermissionGranted() {
 
+                    val myUri = Uri.parse("tel:${mStoreData.phonNum}")
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                    startActivity(myIntent)
+
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+
+                    Toast.makeText(this@ViewStoreDetailActivity, "권한이 거부되어 전화 불가", Toast.LENGTH_SHORT).show()
+
+                }
+
+            }
+
+            TedPermission.create()
+                .setPermissionListener(pl)
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
 
         }
 
